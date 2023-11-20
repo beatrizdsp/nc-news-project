2793dadd6c1036191d7508188c0985cf56ef7764
Beatriz <beatriz@Beatrizs-MacBook-Pro.local>
2023-11-20 11:26:13 +0000

    adds test file

---
+++ __tests__/app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest')
+const app = require('../db/app/app')
+const db = require('../db/seeds/seed')
+
+describe('GET/api/topics',()=>{
+test('GET /api/topics',()=>{
+    
+})
+})
